9404474b84392054fdeb5a03ae0c7638ecfff713
czhu1231 <clairezhu05@gmail.com>
2025-01-24 10:53:49 -0500

    formatting changes

--- frontend/components/percentage/Percentage.js
+++ frontend/components/percentage/Percentage.js
@@ -4,12 +4,12 @@ import './Percentage.css';
 function Percentage(props) {
     const percent = props.percent || Percentage.defaultProps.percent;
     return (
-        <div className = 'percent-result'>
-            <h1 className = 'results-title'>Your Results</h1>
-            <h1 className = 'percent'>{percent}%</h1>
+        <div className='percent-result'>
+            <h1 className='results-title'>Your Results</h1>
+            <h1 className='percent'>{percent}%</h1>
             <h2></h2>
-            <h3>Your music taste is {percent}% similar to other UMD members! Scroll down to see what other 
-            people in your community are listening to...</h3>
+            <h3 className = 'results-text'>Your music taste is {percent}% similar to other UMD members! Scroll down to see what other
+                people in your community are listening to...</h3>
         </div>
     )
 
